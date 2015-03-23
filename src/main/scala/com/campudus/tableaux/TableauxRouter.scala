1ce262b4a23547f32b7ec02256cd6510601e1da3
Sascha Schmidt <sschmid@haw-landshut.de>
2015-03-23 16:52:52 +0100

    Implemented reset with DemoData
    Signed-off-by: Sascha Schmidt <sschmid@haw-landshut.de>

--- src/main/scala/com/campudus/tableaux/TableauxRouter.scala
+++ src/main/scala/com/campudus/tableaux/TableauxRouter.scala
@@ -26,6 +26,32 @@ class TableauxRouter(verticle: Starter) extends Router with VertxAccess {
   val tableId = "/tables/(\\d+)".r
   val controller = new TableauxController(verticle)
 
+  val demoJson = Json.obj(
+    "tableName" -> "Regierungsbezirke Bayern",
+    "columns" -> Json.arr(
+      Json.obj("kind" -> "text", "name" -> "Regierungsbezirk", "ordering" -> 1),
+      Json.obj("kind" -> "text", "name" -> "Hauptstadt", "ordering" -> 2),
+      Json.obj("kind" -> "numeric", "name" -> "Einwohner", "ordering" -> 3),
+      Json.obj("kind" -> "numeric", "name" -> "Fläche in km²", "ordering" -> 4)),
+    "rows" -> Json.arr(
+      Json.obj("values" -> Json.arr("Oberbayern", "München", 4469342, 17530.21)),
+      Json.obj("values" -> Json.arr("Niederbayern", "Landshut", 1189153, 10328.65)),
+      Json.obj("values" -> Json.arr("Oberpfalz", "Regensburg", 1077991, 9690.19)),
+      Json.obj("values" -> Json.arr("Oberfranken", "Bayreuth", 1056365, 7231.49))))
+
+  val demoJson2 = Json.obj(
+    "tableName" -> "Bundesländer Deutschlands",
+    "columns" -> Json.arr(
+      Json.obj("kind" -> "text", "name" -> "Land", "ordering" -> 1),
+      Json.obj("kind" -> "text", "name" -> "Hauptstadt", "ordering" -> 2),
+      Json.obj("kind" -> "numeric", "name" -> "Fläche in km²", "ordering" -> 3),
+      Json.obj("kind" -> "numeric", "name" -> "Einwohner", "ordering" -> 4)),
+    "rows" -> Json.arr(
+      Json.obj("values" -> Json.arr("Baden-Württemberg", "Stuttgart", 35751, 10631278)),
+      Json.obj("values" -> Json.arr("Bayern", "München", 70550, 12604244)),
+      Json.obj("values" -> Json.arr("Berlin", "Berlin", 892, 3440991)),
+      Json.obj("values" -> Json.arr("Brandenburg", "Potsdam", 29655, 2449193))))
+
   def routes(implicit req: HttpServerRequest): PartialFunction[RouteMatch, Reply] = {
     case Get("/") => SendFile("index.html")
     case Get("/tables") => getAsyncReply(GetReturn)(controller.getAllTables())
@@ -34,6 +60,10 @@ class TableauxRouter(verticle: Starter) extends Router with VertxAccess {
     case Get(tableIdRowsId(tableId, rowId)) => getAsyncReply(GetReturn)(controller.getRow(tableId.toLong, rowId.toLong))
     case Get(tableIdColumnsIdRowsId(tableId, columnId, rowId)) => getAsyncReply(GetReturn)(controller.getCell(tableId.toLong, columnId.toLong, rowId.toLong))
     case Post("/reset") => getAsyncReply(SetReturn)(controller.resetDB())
+    case Post("/resetDemo") => getAsyncReply(SetReturn) {
+      controller.createTable(demoJson.getString("tableName"), jsonToSeqOfColumnNameAndType(demoJson), jsonToSeqOfRowsWithValue(demoJson))
+      controller.createTable(demoJson2.getString("tableName"), jsonToSeqOfColumnNameAndType(demoJson2), jsonToSeqOfRowsWithValue(demoJson2))
+    }
     case Post("/tables") => getAsyncReply(SetReturn) {
       getJson(req) flatMap { json =>
         if (json.getFieldNames.contains("columns")) {
