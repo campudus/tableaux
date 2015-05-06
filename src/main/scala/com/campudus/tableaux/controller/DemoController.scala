231d4e977ad1b9626ca9a0ff77ae6a889f170951
Alexander Vetter <vettera@gmail.com>
2015-05-06 10:10:26 +0200

    read demodata from 'demodata/' directory; each json is one table

--- src/main/scala/com/campudus/tableaux/controller/DemoController.scala
+++ src/main/scala/com/campudus/tableaux/controller/DemoController.scala
@@ -4,45 +4,24 @@ package com.campudus.tableaux.controller
  * Controller for resetting the database.
  * Mostly this is need for Demo & Test data.
  */
+
 import com.campudus.tableaux.ArgumentChecker._
-import com.campudus.tableaux.database.Tableaux._
 import com.campudus.tableaux.database._
 import com.campudus.tableaux.database.structure.CreateColumn
 import com.campudus.tableaux.helper.HelperFunctions._
 import com.campudus.tableaux.helper.StandardVerticle
-import org.vertx.scala.core.json.Json
+import org.vertx.scala.core.buffer.Buffer
+import org.vertx.scala.core.json._
 import org.vertx.scala.platform.Verticle
 
-import scala.concurrent.Future
+import scala.concurrent.{Future, Promise}
+import scala.util.{Failure, Success, Try}
 
 class DemoController(val verticle: Verticle, val database: DatabaseConnection) extends DatabaseAccess with StandardVerticle {
-  val demoJson = Json.obj(
-    "tableName" -> "Regierungsbezirke Bayern",
-    "columns" -> Json.arr(
-      Json.obj("kind" -> "text", "name" -> "Regierungsbezirk", "ordering" -> 1),
-      Json.obj("kind" -> "text", "name" -> "Hauptstadt", "ordering" -> 2),
-      Json.obj("kind" -> "numeric", "name" -> "Einwohner", "ordering" -> 3),
-      Json.obj("kind" -> "numeric", "name" -> "Fläche in km²", "ordering" -> 4)),
-    "rows" -> Json.arr(
-      Json.obj("values" -> Json.arr("Oberbayern", "München", 4469342, 17530.21)),
-      Json.obj("values" -> Json.arr("Niederbayern", "Landshut", 1189153, 10328.65)),
-      Json.obj("values" -> Json.arr("Oberpfalz", "Regensburg", 1077991, 9690.19)),
-      Json.obj("values" -> Json.arr("Oberfranken", "Bayreuth", 1056365, 7231.49))))
-
-  val demoJson2 = Json.obj(
-    "tableName" -> "Bundesländer Deutschlands",
-    "columns" -> Json.arr(
-      Json.obj("kind" -> "text", "name" -> "Land", "ordering" -> 1),
-      Json.obj("kind" -> "text", "name" -> "Hauptstadt", "ordering" -> 2),
-      Json.obj("kind" -> "numeric", "name" -> "Fläche in km²", "ordering" -> 3),
-      Json.obj("kind" -> "numeric", "name" -> "Einwohner", "ordering" -> 4)),
-    "rows" -> Json.arr(
-      Json.obj("values" -> Json.arr("Baden-Württemberg", "Stuttgart", 35751, 10631278)),
-      Json.obj("values" -> Json.arr("Bayern", "München", 70550, 12604244)),
-      Json.obj("values" -> Json.arr("Berlin", "Berlin", 892, 3440991)),
-      Json.obj("values" -> Json.arr("Brandenburg", "Potsdam", 29655, 2449193))))
 
   lazy val tableaux = new Tableaux(verticle, database)
+
+  val fileProps = """^(.+[\\/])*(.+)\.(.+)$""".r
 
   def resetDB(): Future[DomainObject] = {
     logger.info("Reset database")
@@ -50,8 +29,62 @@ class DemoController(val verticle: Verticle, val database: DatabaseConnection) e
   }
 
   def createDemoTables(): Future[DomainObject] = {
-    createTable(demoJson.getString("tableName"), jsonToSeqOfColumnNameAndType(demoJson), jsonToSeqOfRowsWithValue(demoJson))
-    createTable(demoJson2.getString("tableName"), jsonToSeqOfColumnNameAndType(demoJson2), jsonToSeqOfRowsWithValue(demoJson2))
+    for {
+      _ <- writeDemoData(readDemoData())
+      emptyObject <- Future.successful(EmptyObject())
+    } yield emptyObject
+  }
+
+  private def writeDemoData(demoData: Future[Seq[JsonObject]]): Future[DomainObject] = {
+    val p = Promise[DomainObject]()
+
+    //TODO It works but I'm not sure if it's nice
+    demoData.map { data =>
+      data.map { table =>
+        createTable(table.getString("tableName"), jsonToSeqOfColumnNameAndType(table), jsonToSeqOfRowsWithValue(table))
+        p.success(EmptyObject())
+      }
+    }
+
+    p.future
+  }
+
+  private def readDemoData(): Future[Seq[JsonObject]] = {
+    readDir("../resources/demodata/", ".*\\.json") flatMap { fileNames =>
+      Future.sequence((fileNames map { file =>
+        readFile(file)
+      }).toSeq)
+    }
+  }
+
+  private def readDir(dir: String, filter: String): Future[Array[String]] = {
+    import org.vertx.scala.core.FunctionConverters._
+
+    val p = Promise[Array[String]]()
+
+    vertx.fileSystem.readDir(dir, filter, {
+      case Success(files) => p.success(files)
+      case Failure(ex) =>
+        logger.info("Failed reading schema directory")
+        p.failure(ex)
+    }: Try[Array[String]] => Unit)
+
+    p.future
+  }
+
+  private def readFile(fileName: String): Future[JsonObject] = {
+    import org.vertx.scala.core.FunctionConverters._
+
+    val p = Promise[JsonObject]()
+
+    vertx.fileSystem.readFile(fileName, {
+      case Success(b) => p.success(Json.fromObjectString(b.toString()))
+      case Failure(ex) =>
+        logger.info("Failed reading schema file")
+        p.failure(ex)
+    }: Try[Buffer] => Unit)
+
+    p.future
   }
 
   private def createTable(tableName: String, columns: => Seq[CreateColumn], rowsValues: Seq[Seq[_]]): Future[DomainObject] = {
