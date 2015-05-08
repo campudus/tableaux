e44ca4379210452c468430e20d9ca3efa8cf8462
Alexander Vetter <vettera@gmail.com>
2015-05-08 16:31:34 +0200

    initialization of VertxAccess members in StandardVerticle must be lazy

--- src/main/scala/com/campudus/tableaux/helper/StandardVerticle.scala
+++ src/main/scala/com/campudus/tableaux/helper/StandardVerticle.scala
@@ -6,9 +6,9 @@ import org.vertx.scala.platform.Verticle
 trait StandardVerticle extends VertxAccess {
   val verticle: Verticle
 
-  val vertx = verticle.vertx
-  val container = verticle.container
-  val logger = verticle.logger
+  lazy val vertx = verticle.vertx
+  lazy val container = verticle.container
+  lazy val logger = verticle.logger
 
   override implicit val executionContext = VertxExecutionContext.fromVertx(vertx, logger)
 }
