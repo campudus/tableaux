1cf6d124e7fb5096ab2258d246e8bd0d2ad41b82
Alexander Vetter <vettera@gmail.com>
2015-06-08 12:48:54 +0200

    fix build fatJar didn't work

--- project/TableauxBuild.scala
+++ project/TableauxBuild.scala
@@ -4,24 +4,17 @@ import sbt._
 import sbt.Keys._
 
 object TableauxBuild extends VertxProject {
-  val module = VertxModule("com.campudus", "tableaux", "0.1.0", "tables into sheets", Some("2.11.2"), Seq("2.10.4", "2.11.2"))
-
-  lazy val copyResourcesFromCore = TaskKey[Unit]("copy-schemas-from-core", "Copy the resources from cms-core into the resources directory")
+  val module = VertxModule("com.campudus", "tableaux", "0.1.0", "tables into sheets", Some("2.11.6"), Seq("2.11.6"))
 
   override lazy val customSettings = Seq(
     libraryDependencies ++= Seq(
+      "joda-time" % "joda-time" % "2.8",
+
       "io.vertx" %% "mod-mysql-postgresql" % "0.3.1" % "provided",
+
       "io.vertx" % "testtools" % "2.0.3-final" % "test",
       "org.hamcrest" % "hamcrest-library" % "1.3" % "test",
       "com.novocode" % "junit-interface" % "0.10" % "test"
-    ),
-    copyResourcesFromCoreTask,
-    copyMod <<= copyMod dependsOn copyResourcesFromCore,
-    runMod <<= runMod dependsOn copyMod
+    )
   )
-
-  lazy val copyResourcesFromCoreTask = copyResourcesFromCore := {
-    implicit val log = streams.value.log
-    copyDirectory((resourceDirectory in Compile).value / "", target.value / "mods" / "resources")
-  }
 }
--- project/VertxProject.scala
+++ project/VertxProject.scala
@@ -10,7 +10,7 @@ case class VertxModule(organization: String,
                        id: String,
                        version: String,
                        description: String,
-                       scalaVersion: Option[String] = Some("2.11.2"),
+                       scalaVersion: Option[String] = Some("2.11.6"),
                        crossScalaVersions: Seq[String] = Seq.empty)
 
 
@@ -26,6 +26,7 @@ trait VertxProject extends Build {
   lazy val fatJar = TaskKey[Unit]("fat-jar", "Creates a fat jar file for deployment on the Server")
   lazy val runnableModule = TaskKey[Unit]("runnable-module", "The module is ready to use here")
   lazy val runMod = TaskKey[Unit]("run-mod", "runs the module")
+  lazy val copyMainResources = TaskKey[Unit]("copy-schemas-from-core", "Copy the resources from main into the resources directory")
 
   def customSettings: Seq[Setting[_]] = Seq.empty
 
@@ -57,7 +58,7 @@ trait VertxProject extends Build {
     ) ++ module.scalaVersion.map(_ => "io.vertx" %% "lang-scala" % vertxScalaVersion % "provided").toList,
     libraryDependencies ++= dependencies,
     libraryDependencies in fatJar += "io.vertx" % "vertx-hazelcast" % vertxVersion,
-    //      libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _),
+
     // Fork JVM to allow Scala in-flight compilation tests to load the Scala interpreter
     fork in Test := true,
     // Vert.x tests are not designed to run in paralell
@@ -67,7 +68,7 @@ trait VertxProject extends Build {
     javaOptions in Test += "-Ddebug.basedir=" + baseDirectory.value,
     javaOptions in Test += "-Ddebug.target=" + target.value,
     // Adjust test system properties so that scripts are found
-    javaOptions in Test += "-Dvertx.test.resources=src/test/scripts",
+    javaOptions in Test += "-Dvertx.test.resources=src/test/resources",
     // Adjust test modules directory
     javaOptions in Test += "-Dvertx.clusterManagerFactory=org.vertx.java.spi.cluster.impl.hazelcast.HazelcastClusterManagerFactory",
     javaOptions in Test += "-Dvertx.mods=mods",
@@ -78,11 +79,20 @@ trait VertxProject extends Build {
       val file = (resourceManaged in Compile).value / "langs.properties"
 
       val scala = s"scala=io.vertx~lang-scala_${getMajor(scalaVersion.value)}~$vertxScalaVersion:org.vertx.scala.platform.impl.ScalaVerticleFactory\n.scala=scala\n"
-      IO.write(file, scala, StandardCharsets.UTF_8)
       val javascript = s"rhino=io.vertx~lang-rhino~2.1.1:org.vertx.java.platform.impl.RhinoVerticleFactory\n.js=rhino\n"
+
+      IO.write(file, scala, StandardCharsets.UTF_8)
       IO.append(file, javascript, StandardCharsets.UTF_8)
 
-      Seq(file)
+      val platformLibDir = (resourceManaged in Compile).value / "platform_lib"
+      IO.createDirectory(platformLibDir)
+
+      val platformLibFile = platformLibDir / "langs.properties"
+
+      IO.write(platformLibFile, scala, StandardCharsets.UTF_8)
+      IO.append(platformLibFile, javascript, StandardCharsets.UTF_8)
+
+      Seq(file, platformLibFile)
     }.taskValue
   }.toList ++ Seq(
     // Publishing settings
@@ -104,9 +114,16 @@ trait VertxProject extends Build {
     runModTask,
     pullInDepsTask,
     fatJarTask,
+    copyMainResourcesTask,
+
     copyMod <<= copyMod dependsOn (copyResources in Compile),
-    (test in Test) <<= (test in Test) dependsOn copyMod,
+    copyMod <<= copyMod dependsOn copyMainResources,
+
+    runMod <<= runMod dependsOn copyMod,
     zipMod <<= zipMod dependsOn copyMod,
+    fatJar <<= fatJar dependsOn (copyMod, pullInDeps),
+
+    (test in Test) <<= (test in Test) dependsOn copyMod,
     (packageBin in Compile) <<= (packageBin in Compile) dependsOn copyMod
   )
 
@@ -129,12 +146,16 @@ trait VertxProject extends Build {
     log.info("Create module " + moduleName)
     createDirectory(moduleDir)
     copyDirectory((classDirectory in Compile).value, moduleDir)
+
     val libDir = moduleDir / "lib"
     createDirectory(libDir)
     // Get the runtime classpath to get all dependencies except provided ones
-    (managedClasspath in Runtime).value foreach { classpathEntry =>
+    val classpath = (managedClasspath in Runtime).value
+
+    classpath filter { e => !e.data.name.contains("scala-library")} foreach { classpathEntry =>
       copyClasspathFile(classpathEntry, libDir)
     }
+
     moduleDir
   }
 
@@ -187,6 +208,11 @@ trait VertxProject extends Build {
     System.setProperty("vertx.modulename", moduleName)
     log.info(s"args=$args, cp=$cp")
     toError(r.run("org.vertx.java.platform.impl.cli.Starter", cp.map(_.data), args, log))
+  }
+
+  lazy val copyMainResourcesTask = copyMainResources := {
+    implicit val log = streams.value.log
+    copyDirectory((resourceDirectory in Compile).value / "", target.value / "mods" / "resources")
   }
 
   private def getMajor(version: String): String = version.substring(0, version.lastIndexOf('.'))
