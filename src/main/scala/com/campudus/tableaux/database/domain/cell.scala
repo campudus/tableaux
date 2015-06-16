a3b78aaab98522754bdc8339544fdd0633fde845
Alexander Vetter <vettera@gmail.com>
2015-06-16 10:49:42 +0200

    refactor cell serialization

--- src/main/scala/com/campudus/tableaux/database/domain/cell.scala
+++ src/main/scala/com/campudus/tableaux/database/domain/cell.scala
@@ -1,15 +1,16 @@
 package com.campudus.tableaux.database.domain
 
-import com.campudus.tableaux.database.model.TableauxModel
-import TableauxModel._
+import com.campudus.tableaux.database.model.TableauxModel._
 import org.vertx.scala.core.json._
 
 case class Cell[A, B <: ColumnType[A]](column: B, rowId: IdType, value: A) extends DomainObject {
   def getJson: JsonObject = {
     val v = value match {
-      case link: Link[A] => link.toJson
+      case s: Seq[_] => s map { case d: DomainObject => d.getJson }
+      case d: DomainObject => d.getJson
       case _ => value
     }
+
     Json.obj("rows" -> Json.arr(Json.obj("value" -> v)))
   }
 
