997ac011107225b22ac760c1bd2ff9935dcfe0a5
Alexander Vetter <vettera@gmail.com>
2015-06-16 10:51:06 +0200

    Continued development...

--- src/main/scala/com/campudus/tableaux/database/domain/link.scala
+++ src/main/scala/com/campudus/tableaux/database/domain/link.scala
@@ -4,6 +4,7 @@ import com.campudus.tableaux.database.model.TableauxModel
 import TableauxModel._
 import org.vertx.scala.core.json._
 
-case class Link[A](id: IdType, value: A) {
-  def toJson: JsonObject = Json.obj("id" -> id, "value" -> value)
+case class Link[A](id: IdType, value: A) extends DomainObject {
+  override def getJson: JsonObject = Json.obj("id" -> id, "value" -> value)
+  override def setJson: JsonObject = getJson
 }
