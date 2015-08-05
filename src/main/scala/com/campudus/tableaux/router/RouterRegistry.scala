a1359712ae692c3cb63808c45e60ccd6c797e66e
Alexander Vetter <vettera@gmail.com>
2015-08-05 13:33:50 +0200

    refactor RouterRegistry

--- src/main/scala/com/campudus/tableaux/router/RouterRegistry.scala
+++ src/main/scala/com/campudus/tableaux/router/RouterRegistry.scala
@@ -1,12 +1,13 @@
 package com.campudus.tableaux.router
 
 import com.campudus.tableaux.TableauxConfig
-import com.campudus.tableaux.controller.{StructureController, MediaController, SystemController, TableauxController}
+import com.campudus.tableaux.controller.{MediaController, StructureController, SystemController, TableauxController}
 import com.campudus.tableaux.database.DatabaseConnection
 import com.campudus.tableaux.database.model._
 import org.vertx.scala.core.http.HttpServerRequest
 import org.vertx.scala.platform.Verticle
-import org.vertx.scala.router.routing.{Get, SendFile}
+import org.vertx.scala.router.RouterException
+import org.vertx.scala.router.routing.{Error, Get, SendFile}
 
 object RouterRegistry {
   def apply(tableauxConfig: TableauxConfig, dbConnection: DatabaseConnection): RouterRegistry = {
@@ -17,34 +18,33 @@ object RouterRegistry {
     val fileModel = FileModel(dbConnection)
     val structureModel = StructureModel(dbConnection)
 
-    new RouterRegistry(
-      tableauxConfig,
-
+    val routers = Seq(
       SystemRouter(tableauxConfig, SystemController(_, systemModel, tableauxModel, structureModel)),
       TableauxRouter(tableauxConfig, TableauxController(_, tableauxModel)),
       MediaRouter(tableauxConfig, MediaController(_, folderModel, fileModel)),
       StructureRouter(tableauxConfig, StructureController(_, structureModel))
     )
+
+    new RouterRegistry(tableauxConfig, routers)
   }
 }
 
-class RouterRegistry(override val config: TableauxConfig,
-                     val systemRouter: SystemRouter,
-                     val tableauxRouter: TableauxRouter,
-                     val mediaRouter: MediaRouter,
-                     val structureRouter: StructureRouter) extends BaseRouter {
+class RouterRegistry(override val config: TableauxConfig, val routers: Seq[BaseRouter]) extends BaseRouter {
 
   override val verticle: Verticle = config.verticle
 
   override def routes(implicit req: HttpServerRequest): Routing = {
-    myRoutes orElse
-      systemRouter.routes orElse
-      tableauxRouter.routes orElse
-      mediaRouter.routes orElse
-      structureRouter.routes
+    routers.map(_.routes).foldLeft(defaultRoutes)({
+      case (last, current) =>
+        last orElse current
+    }) orElse noRouteFound
   }
 
-  def myRoutes(implicit req: HttpServerRequest): Routing = {
+  def defaultRoutes(implicit req: HttpServerRequest): Routing = {
     case Get("/") | Get("/index.html") => SendFile("index.html")
+  }
+
+  def noRouteFound(implicit req: HttpServerRequest): Routing = {
+    case _ => Error(RouterException(message = "No route found", statusCode = 404))
   }
 }
