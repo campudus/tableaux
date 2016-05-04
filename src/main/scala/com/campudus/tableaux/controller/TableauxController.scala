5eea2f483da7c15c01fe655e7eabeb83970948bb
Alexander Vetter <vettera@gmail.com>
2016-05-04 10:23:17 +0200

    Invalidate on attachment delete

--- src/main/scala/com/campudus/tableaux/controller/TableauxController.scala
+++ src/main/scala/com/campudus/tableaux/controller/TableauxController.scala
@@ -4,6 +4,7 @@ import java.util.UUID
 
 import com.campudus.tableaux.ArgumentChecker._
 import com.campudus.tableaux.TableauxConfig
+import com.campudus.tableaux.cache.CacheClient
 import com.campudus.tableaux.database.domain._
 import com.campudus.tableaux.database.model.TableauxModel._
 import com.campudus.tableaux.database.model.{Attachment, TableauxModel}
@@ -128,6 +129,7 @@ class TableauxController(override val config: TableauxConfig, override protected
     //TODO introduce a Cell identifier with tableId, columnId and rowId
     for {
       _ <- repository.attachmentModel.delete(Attachment(tableId, columnId, rowId, UUID.fromString(uuid), None))
+      _ <- CacheClient(vertx).invalidateCellValue(tableId, columnId, rowId)
     } yield EmptyObject()
   }
 
