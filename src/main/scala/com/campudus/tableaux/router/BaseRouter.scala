c94bf21eda94534defb27579ea389a073773e3ad
Alexander Vetter <vettera@gmail.com>
2016-07-19 09:55:25 +0200

    Test for invalid JSON

--- src/main/scala/com/campudus/tableaux/router/BaseRouter.scala
+++ src/main/scala/com/campudus/tableaux/router/BaseRouter.scala
@@ -6,6 +6,7 @@ import com.campudus.tableaux.database.{EmptyReturn, GetReturn, ReturnType}
 import com.campudus.tableaux.helper.VertxAccess
 import com.typesafe.scalalogging.LazyLogging
 import io.vertx.core.buffer.Buffer
+import io.vertx.core.json.DecodeException
 import io.vertx.ext.web.RoutingContext
 import io.vertx.scala.FunctionConverters._
 import io.vertx.scala.FutureHelper._
@@ -66,22 +67,35 @@ trait BaseRouter extends Router with VertxAccess with LazyLogging {
   }
 
   def getJson(context: RoutingContext): Future[JsonObject] = futurify { p: Promise[JsonObject] =>
-    context.request().bodyHandler({ buffer: Buffer =>
-      val requestBody = buffer.toString()
-      Option(requestBody).getOrElse("").isEmpty match {
-        case true => p.failure(NoJsonFoundException("No JSON found."))
-        case false => requestBody match {
-          case "null" => p.success(Json.emptyObj())
-          case _ => Try(Json.fromObjectString(requestBody)) match {
-            case Success(r) =>
-              p.success(r)
-            case Failure(x) =>
-              logger.error(s"Couldn't parse requestBody. Excepted JSON but got: [$requestBody]")
-              p.failure(x)
-          }
+    context.request().bodyHandler(parseRequestBuffer(p)(_: Buffer))
+  }
+
+  private def parseRequestBuffer(p: Promise[JsonObject])(buffer: Buffer): Unit = {
+    val requestBody = buffer.toString()
+    Option(requestBody).getOrElse("").isEmpty match {
+      case true =>
+        p.failure(NoJsonFoundException("No JSON found."))
+
+      case false =>
+        requestBody match {
+          case "null" =>
+            p.success(Json.emptyObj())
+
+          case _ =>
+            Try(Json.fromObjectString(requestBody)) match {
+              case Success(r) =>
+                p.success(r)
+
+              case Failure(ex: DecodeException) =>
+                logger.error(s"Couldn't parse requestBody. JSON is valid: [$requestBody]")
+                p.failure(InvalidJsonException(ex.getMessage, "invalid"))
+
+              case Failure(ex) =>
+                logger.error(s"Couldn't parse requestBody. Excepted JSON but got: [$requestBody]")
+                p.failure(ex)
+            }
         }
-      }
-    })
+    }
   }
 
   def getLongParam(name: String, context: RoutingContext): Option[Long] = {
