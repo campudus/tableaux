630dd14bc3fd51b230c63b634336ef712bf3289f
Manfred Zingl <mz@campudus.com>
2019-01-03 11:11:34 +0100

    added history event types for annotations

--- src/main/scala/com/campudus/tableaux/database/historyEventType.scala
+++ src/main/scala/com/campudus/tableaux/database/historyEventType.scala
@@ -8,12 +8,16 @@ sealed trait HistoryEventType {
 object HistoryEventType {
   final val CELL_CHANGED = "cell_changed"
   final val ROW_CREATED = "row_created"
+  final val ANNOTATION_ADDED = "annotation_added"
+  final val ANNOTATION_REMOVED = "annotation_removed"
 
   def apply(eventType: String): HistoryEventType = {
-    Option(eventType) match {
-      case Some(CELL_CHANGED) => CellChangedEvent
-      case Some(ROW_CREATED) => RowCreatedEvent
-      case _ => throw new IllegalArgumentException(s"Invalid argument for HistoryEventType: '$eventType'")
+    eventType match {
+      case CELL_CHANGED => CellChangedEvent
+      case ROW_CREATED => RowCreatedEvent
+      case ANNOTATION_ADDED => RowCreatedEvent
+      case ANNOTATION_REMOVED => RowCreatedEvent
+      case _ => throw new IllegalArgumentException(s"Invalid argument for HistoryEventType $eventType")
     }
   }
 }
@@ -24,4 +28,12 @@ case object CellChangedEvent extends HistoryEventType {
 
 case object RowCreatedEvent extends HistoryEventType {
   override val eventName = HistoryEventType.ROW_CREATED
+}
+
+case object AnnotationAddedEvent extends HistoryEventType {
+  override val eventName = HistoryEventType.ANNOTATION_ADDED
+}
+
+case object AnnotationRemovedEvent extends HistoryEventType {
+  override val eventName = HistoryEventType.ANNOTATION_REMOVED
 }
