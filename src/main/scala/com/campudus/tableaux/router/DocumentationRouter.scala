dedab35181a7f3a3d4d5575c154620409fd705f6
Manfred Zingl <mz@campudus.com>
2019-02-01 13:28:25 +0100

    changed DocumentationRouter and MediaRouter to vertx3 handling

--- src/main/scala/com/campudus/tableaux/router/DocumentationRouter.scala
+++ src/main/scala/com/campudus/tableaux/router/DocumentationRouter.scala
@@ -3,12 +3,11 @@ package com.campudus.tableaux.router
 import java.net.URL
 
 import com.campudus.tableaux.TableauxConfig
-import com.campudus.tableaux.helper.DocUriParser
+import com.campudus.tableaux.helper._
 import io.vertx.scala.core.http.HttpServerRequest
-import io.vertx.scala.ext.web.RoutingContext
+import io.vertx.scala.ext.web.{Router, RoutingContext}
 
 import scala.io.Source
-import scala.util.matching.Regex
 
 object DocumentationRouter {
 
@@ -21,13 +20,80 @@ class DocumentationRouter(override val config: TableauxConfig) extends BaseRoute
 
   val swaggerUiVersion = "3.17.6"
 
-  val Index: Regex = "^/docs/index.html$".r
-  val IndexRedirect: Regex = "^/docs$|^/docs/$".r
+  val Index: String = "/index.html"
+  val Swagger: String = "/swagger.json"
 
-  val Swagger: Regex = "^/docs/swagger\\.json$".r
+  val File: String = s"""/(?<file>[A-Za-z0-9-_\\.]{1,60}){1}"""
+  val FileWithDirectory: String = s"""/(?<directory>[A-Za-z0-9-_\\.]{1,60}){1}/(?<file>[A-Za-z0-9-_\\.]{1,60}){1}"""
 
-  val OtherFile: Regex = "^/docs/([A-Za-z0-9-_\\.]{1,60}){1}$".r
-  val OtherFileWithDirectory: Regex = "^/docs/([A-Za-z0-9-_\\.]{1,60}){1}/([A-Za-z0-9-_\\.]{1,60}){1}$".r
+  def route: Router = {
+    val router = Router.router(vertx)
+
+    router.get(Index).handler(index)
+    router.get("/").handler(indexRedirect)
+    router.get(Swagger).handler(getSwagger)
+    router.getWithRegex(File).handler(getFile)
+    router.getWithRegex(FileWithDirectory).handler(getFileWithDirectory)
+
+    router
+  }
+
+  private def index(context: RoutingContext): Unit = {
+    val (scheme, host, basePath) = parseAbsoluteURI(context.request())
+
+    val is = getClass.getResourceAsStream(s"/META-INF/resources/webjars/swagger-ui/$swaggerUiVersion/index.html")
+
+    val swaggerURL = new URL(new URL(s"$scheme://$host"), basePath + "/docs/swagger.json")
+
+    val file = Source
+      .fromInputStream(is, "UTF-8")
+      .mkString
+      .replace("https://petstore.swagger.io/v2/swagger.json", swaggerURL.toString)
+
+    logger.info(s"Headers ${context.request().headers().asJava.asInstanceOf[io.vertx.core.MultiMap].entries()}")
+    logger.info(s"Swagger ${context.request().absoluteURI()} => ($scheme, $host, $basePath) => $swaggerURL")
+
+    sendReply(context, OkString(file, "text/html; charset=UTF-8"))
+  }
+
+  private def indexRedirect(context: RoutingContext): Unit = {
+    val (_, _, basePath) = parseAbsoluteURI(context.request())
+    val path = List(basePath, "docs", "index.html")
+      .flatMap({
+        case str if str.isEmpty => None
+        case str => Option(str)
+      })
+      .mkString("/")
+
+    sendReply(context, StatusCode(301, Header("Location", s"/$path", NoBody)))
+  }
+
+  private def getSwagger(context: RoutingContext): Unit = {
+    val (scheme, host, basePath) = parseAbsoluteURI(context.request())
+
+    val is = getClass.getResourceAsStream(s"/swagger.json")
+    val file = Source.fromInputStream(is, "UTF-8").mkString
+
+    val json = file
+      .replace("$SCHEME$", scheme)
+      .replace("$HOST$", host)
+      .replace("$BASEPATH$", if (basePath.startsWith("/")) basePath else s"/$basePath")
+
+    sendReply(context, OkString(json, "application/javascript; charset=UTF-8"))
+  }
+
+  private def getFile(context: RoutingContext): Unit = {
+    for {
+      file <- getStringParam("file", context)
+    } yield sendReply(context, SendEmbeddedFile(s"/META-INF/resources/webjars/swagger-ui/$swaggerUiVersion/$file"))
+  }
+  private def getFileWithDirectory(context: RoutingContext): Unit = {
+    for {
+      directory <- getStringParam("directory", context)
+      file <- getStringParam("file", context)
+    } yield
+      sendReply(context, SendEmbeddedFile(s"/META-INF/resources/webjars/swagger-ui/$swaggerUiVersion/$directory/$file"))
+  }
 
   private def parseAbsoluteURI(request: HttpServerRequest): (String, String, String) = {
 
@@ -59,56 +125,4 @@ class DocumentationRouter(override val config: TableauxConfig) extends BaseRoute
 
     DocUriParser.parse(uri)
   }
-
-//  override def routes(implicit context: RoutingContext): Routing = {
-//    case Get(IndexRedirect()) =>
-//      val (_, _, basePath) = parseAbsoluteURI(context.request())
-//      val path = List(basePath, "docs", "index.html")
-//        .flatMap({
-//          case str if str.isEmpty => None
-//          case str => Option(str)
-//        })
-//        .mkString("/")
-//
-//      StatusCode(301, Header("Location", s"/$path", NoBody))
-//
-//    case Get(Index()) =>
-//      val (scheme, host, basePath) = parseAbsoluteURI(context.request())
-//
-//      val is = getClass.getResourceAsStream(s"/META-INF/resources/webjars/swagger-ui/$swaggerUiVersion/index.html")
-//
-//      val swaggerURL = new URL(new URL(s"$scheme://$host"), basePath + "/docs/swagger.json")
-//
-//      val file = Source
-//        .fromInputStream(is, "UTF-8")
-//        .mkString
-//        .replace(
-//          "https://petstore.swagger.io/v2/swagger.json",
-//          swaggerURL.toString
-//        )
-//
-//      logger.info(s"Headers ${context.request().headers().asJava.asInstanceOf[io.vertx.core.MultiMap].entries()}")
-//      logger.info(s"Swagger ${context.request().absoluteURI()} => ($scheme, $host, $basePath) => $swaggerURL")
-//
-//      OkString(file, "text/html; charset=UTF-8")
-//
-//    case Get(Swagger()) =>
-//      val (scheme, host, basePath) = parseAbsoluteURI(context.request())
-//
-//      val is = getClass.getResourceAsStream(s"/swagger.json")
-//      val file = Source.fromInputStream(is, "UTF-8").mkString
-//
-//      val json = file
-//        .replace("$SCHEME$", scheme)
-//        .replace("$HOST$", host)
-//        .replace("$BASEPATH$", if (basePath.startsWith("/")) basePath else s"/$basePath")
-//
-//      OkString(json, "application/javascript; charset=UTF-8")
-//
-//    case Get(OtherFile(file)) =>
-//      SendEmbeddedFile(s"/META-INF/resources/webjars/swagger-ui/$swaggerUiVersion/$file")
-//
-//    case Get(OtherFileWithDirectory(directory, file)) =>
-//      SendEmbeddedFile(s"/META-INF/resources/webjars/swagger-ui/$swaggerUiVersion/$directory/$file")
-//  }
 }
--- src/main/scala/com/campudus/tableaux/router/RouterRegistry.scala
+++ src/main/scala/com/campudus/tableaux/router/RouterRegistry.scala
@@ -1,14 +1,16 @@
 package com.campudus.tableaux.router
 
-import com.campudus.tableaux.{InvalidRequestException, TableauxConfig}
+import com.campudus.tableaux.TableauxConfig
 import com.campudus.tableaux.controller.{MediaController, StructureController, SystemController, TableauxController}
 import com.campudus.tableaux.database.DatabaseConnection
 import com.campudus.tableaux.database.model._
-import io.vertx.scala.ext.web.{Router, RoutingContext}
+import io.vertx.scala.ext.web.Router
 
 object RouterRegistry {
 
-  def apply(tableauxConfig: TableauxConfig, dbConnection: DatabaseConnection, mainRouter: Router): RouterRegistry = {
+  def init(tableauxConfig: TableauxConfig, dbConnection: DatabaseConnection): Router = {
+
+    val mainRouter: Router = Router.router(tableauxConfig.vertx)
 
     val systemModel = SystemModel(dbConnection)
     val structureModel = StructureModel(dbConnection)
@@ -19,23 +21,21 @@ object RouterRegistry {
 
     val systemRouter = SystemRouter(tableauxConfig, SystemController(_, systemModel, tableauxModel, structureModel))
     val tableauxRouter = TableauxRouter(tableauxConfig, TableauxController(_, tableauxModel))
-//      val mediaRouter = MediaRouter(tableauxConfig, MediaController(_, folderModel, fileModel, attachmentModel))
+    val mediaRouter = MediaRouter(tableauxConfig, MediaController(_, folderModel, fileModel, attachmentModel))
     val structureRouter = StructureRouter(tableauxConfig, StructureController(_, structureModel))
-//      val documentationRouter = DocumentationRouter(tableauxConfig)
-
-    val routerRegistry = new RouterRegistry(tableauxConfig)
+    val documentationRouter = DocumentationRouter(tableauxConfig)
 
     mainRouter.mountSubRouter("/system", systemRouter.route)
     mainRouter.mountSubRouter("/", structureRouter.route)
     mainRouter.mountSubRouter("/", tableauxRouter.route)
+    mainRouter.mountSubRouter("/", mediaRouter.route)
+    mainRouter.mountSubRouter("/docs", documentationRouter.route)
 
-    mainRouter.get("/").handler(routerRegistry.defaultRoute)
+    mainRouter.get("/").handler(systemRouter.defaultRoute)
     mainRouter.get("/index.html").handler(systemRouter.defaultRoute)
 
     mainRouter.route().handler(systemRouter.noRouteMatched)
 
-    routerRegistry
+    mainRouter
   }
 }
-
-class RouterRegistry(val config: TableauxConfig) extends BaseRouter {}
