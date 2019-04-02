298b4f7cefe82e295715207a018f71e62c9eee8d
Manfred Zingl <mz@campudus.com>
2019-04-02 18:07:18 +0200

    draft for auth

--- src/main/scala/com/campudus/tableaux/router/RouterRegistry.scala
+++ src/main/scala/com/campudus/tableaux/router/RouterRegistry.scala
@@ -4,8 +4,15 @@ import com.campudus.tableaux.controller.{MediaController, StructureController, S
 import com.campudus.tableaux.database.DatabaseConnection
 import com.campudus.tableaux.database.model._
 import com.campudus.tableaux.{RequestContext, TableauxConfig}
-import io.vertx.scala.ext.web.handler.CookieHandler
+import io.vertx.ext.auth.oauth2.OAuth2FlowType
+import io.vertx.lang.scala.json.JsonObject
+import io.vertx.scala.ext.auth.oauth2.OAuth2Auth
+import io.vertx.scala.ext.auth.oauth2.providers.KeycloakAuth
+import io.vertx.scala.ext.web.handler.{CookieHandler, OAuth2AuthHandler}
 import io.vertx.scala.ext.web.{Router, RoutingContext}
+import org.vertx.scala.core.json.Json
+
+import scala.util.{Failure, Success}
 
 object RouterRegistry {
 
@@ -14,6 +21,43 @@ object RouterRegistry {
     val mainRouter: Router = Router.router(tableauxConfig.vertx)
 
     implicit val requestContext: RequestContext = RequestContext()
+
+    // you would get this config from the keycloak admin console
+    var keycloakJson = Json.fromObjectString(
+      """
+        |{
+        |  "realm": "master",
+        |  "auth-server-url": "http://localhost:9999/auth",
+        |  "ssl-required": "external",
+        |  "resource": "grud",
+        |  "credentials": {
+        |    "secret": "bb497817-294e-4abd-b34c-307ea6c36a64"
+        |  },
+        |  "confidential-port": 0
+        |}
+      """.stripMargin
+    )
+
+    // Initialize the OAuth2 Library
+//    var oauth2 = KeycloakAuth.create(tableauxConfig.vertx, OAuth2FlowType.PASSWORD, keycloakJson)
+
+//    // first get a token (authenticate)
+//    oauth2
+//      .authenticateFuture(new io.vertx.core.json.JsonObject().put("username", "user").put("password", "secret"))
+//      .onComplete {
+//        case Success(result) => {}
+//        case Failure(cause) => {
+//          println(s"$cause")
+//        }
+//      }
+
+    val oauth2 = OAuth2AuthHandler.create(
+      OAuth2Auth.createKeycloak(tableauxConfig.vertx, OAuth2FlowType.AUTH_CODE, keycloakJson),
+      "http://localhost:8080/docs")
+
+    oauth2.setupCallback(mainRouter.get("/docs"))
+
+    mainRouter.route().handler(oauth2)
 
     // This cookie handler will be called for all routes
     mainRouter.route().handler(CookieHandler.create())
