9b3a65fd064af6a1a99f3c068a304c4922d62f65
Manfred Zingl <mz@campudus.com>
2019-06-05 11:31:05 +0200

    added some authorization tests

--- src/test/scala/com/campudus/tableaux/testtools/TableauxTestBase.scala
+++ src/test/scala/com/campudus/tableaux/testtools/TableauxTestBase.scala
@@ -165,13 +165,8 @@ trait TableauxTestBase
 
   override var tableauxConfig: TableauxConfig = _
 
-  val wildcardAccessToken = "" +
-    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjYW1wdWR1cy10ZXN0Iiwic3ViIjoidGVzdEBjYW1wdWR1cy5jb20iLCJhdWQiOiJnc" +
-    "nVkLWJhY2tlbmQiLCJuYmYiOjAsImlhdCI6MTU1NzMyODIwNywiZXhwIjoyMjIyMjIyMjIyLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJ1bml0LXRlc3R" +
-    "lciIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiZGV2ZWxvcGVyIiwidmlldy10YWJsZXMiLCJkZWxldGUtbWVkaWEiLCJ2aWV3L" +
-    "WNlbGxzIl19LCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJuYW1lIjoiVGVzdCBUZXN0IiwicHJlZmVycmVkX3VzZXJuYW1lIjoiVGVzdCIsImdpdmV" +
-    "uX25hbWUiOiJUZXN0IiwiZmFtaWx5X25hbWUiOiJUZXN0In0.YrJ4ikXxjRBITp9B98lc-ygr7Xlc52PKSCnSU1G3YWOxec9DJH0ybkGdwSYqVLejQ" +
-    "5PC12CVlh19IAEHON2lXTPmMAMOoOlG5dcvTs6MSYnYwoJnTE91MJ0yUJHRcmSkC6npbYnsYjwzk_UKgXcmKYW6UMrsIcU1bEImXWNoLtU"
+  // default access token used for all integration tests if no explicit token is provided
+  var wildcardAccessToken: String = _
 
   @Before
   def before(context: TestContext): Unit = {
@@ -227,6 +222,11 @@ trait TableauxTestBase
     vertx
       .deployVerticleFuture(ScalaVerticle.nameForVerticle[Starter], options)
       .onComplete(completionHandler)
+
+    val tokenHelper = TokenHelper(this.vertxAccess())
+
+    wildcardAccessToken = tokenHelper.generateToken(
+      Json.obj("aud" -> "grud-backend", "iss" -> "campudus-test", "preferred_username" -> "Test"))
   }
 
   @After
