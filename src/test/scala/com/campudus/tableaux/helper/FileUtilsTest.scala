d6d5dc48e3ce3801fe0df6d53e9ba1654b17c097
Manfred Zingl <mz@campudus.com>
2019-06-06 16:38:41 +0200

    added rolePermission config and json file loader

--- src/test/scala/com/campudus/tableaux/helper/FileUtilsTest.scala
+++ src/test/scala/com/campudus/tableaux/helper/FileUtilsTest.scala
@@ -1,37 +1,17 @@
 package com.campudus.tableaux.helper
 
-import com.campudus.tableaux.TableauxConfig
-import com.campudus.tableaux.testtools.{TestAssertionHelper, TestConfig}
+import java.io.FileNotFoundException
+
+import com.campudus.tableaux.testtools.TableauxTestBase
 import io.vertx.ext.unit.TestContext
 import io.vertx.ext.unit.junit.VertxUnitRunner
-import io.vertx.scala.core.Vertx
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.vertx.scala.core.json.JsonObject
 
-import scala.concurrent.Future
 import scala.reflect.io.Path
-import scala.util.{Failure, Success}
 
 @RunWith(classOf[VertxUnitRunner])
-class FileUtilsTest extends VertxAccess with TestConfig with TestAssertionHelper {
-
-  override val vertx: Vertx = Vertx.vertx()
-
-  def okTest(f: => Future[_])(implicit context: TestContext): Unit = {
-    val async = context.async()
-    (try {
-      f
-    } catch {
-      case ex: Throwable => Future.failed(ex)
-    }) onComplete {
-      case Success(_) => async.complete()
-      case Failure(ex) =>
-        logger.error("failed test", ex)
-        context.fail(ex)
-        async.complete()
-    }
-  }
+class FileUtilsTest extends TableauxTestBase {
 
   @Test
   def testMakeSameDirectoriesTwiceShouldNotFail(implicit context: TestContext): Unit = {
@@ -40,9 +20,9 @@ class FileUtilsTest extends VertxAccess with TestConfig with TestAssertionHelper
       val path = Path(uploadsDirectory) / "test" / "asdf"
 
       for {
-        _ <- FileUtils(this).mkdirs(path)
+        _ <- FileUtils(this.vertxAccess()).mkdirs(path)
 
-        _ <- FileUtils(this).mkdirs(path)
+        _ <- FileUtils(this.vertxAccess()).mkdirs(path)
 
         exists <- vertx.fileSystem().existsFuture(path.toString())
 
@@ -54,9 +34,34 @@ class FileUtilsTest extends VertxAccess with TestConfig with TestAssertionHelper
     }
   }
 
-  override var host: String = _
-  override var port: Int = _
-  override var databaseConfig: JsonObject = _
-  override var authConfig: JsonObject = _
-  override var tableauxConfig: TableauxConfig = _
+  @Test
+  def testLoadNonExistingJsonFileShouldFail(implicit context: TestContext): Unit = {
+    okTest {
+      val jsonFilePath = "./does_not_exist.json"
+
+      for {
+        isException: Boolean <- FileUtils(this.vertxAccess())
+          .asyncReadJsonFile(jsonFilePath)
+          .map(_ => false)
+          .recover({
+            case _: FileNotFoundException => true
+          })
+      } yield {
+        assertEquals(isException, true)
+      }
+    }
+  }
+
+  @Test
+  def testLoadJsonFileShouldNotFail(implicit context: TestContext): Unit = {
+    okTest {
+      val jsonFilePath = "./conf-example.json"
+
+      for {
+        json <- FileUtils(this.vertxAccess()).asyncReadJsonFile(jsonFilePath)
+      } yield {
+        assertEquals(json.getInteger("port"), 8080)
+      }
+    }
+  }
 }
