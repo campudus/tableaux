4bb8b9dbaf53b999ade1e2b4bbd4052ee93c61c6
Manfred Zingl <mz@campudus.com>
2019-07-09 10:57:09 +0200

    auth check retrieveColumns  - add filter for parent object (tables)

--- src/main/scala/com/campudus/tableaux/router/auth/permission/ComparisonObjects.scala
+++ src/main/scala/com/campudus/tableaux/router/auth/permission/ComparisonObjects.scala
@@ -5,19 +5,19 @@ import com.campudus.tableaux.database.domain.{ColumnType, Table}
 object ComparisonObjects {
 
   def apply(): ComparisonObjects = {
-    new ComparisonObjects(None, None)
+    new ComparisonObjects()
   }
 
   def apply(table: Table): ComparisonObjects = {
-    new ComparisonObjects(tableOpt = Some(table), None)
+    new ComparisonObjects(Some(table))
   }
 
   def apply(table: Table, column: ColumnType[_]): ComparisonObjects = {
-    new ComparisonObjects(tableOpt = Some(table), columnOpt = Some(column))
+    new ComparisonObjects(Some(table), Some(column))
   }
 
   def apply(column: ColumnType[_]): ComparisonObjects = {
-    new ComparisonObjects(tableOpt = None, columnOpt = Some(column))
+    new ComparisonObjects(columnOpt = Some(column))
   }
 }
 
@@ -26,9 +26,14 @@ object ComparisonObjects {
   * any comparison object, while ScopeCell can have a Table, a Column and a Langtag.
   */
 case class ComparisonObjects(
-    tableOpt: Option[Table],
-    columnOpt: Option[ColumnType[_]]
+    tableOpt: Option[Table] = None,
+    columnOpt: Option[ColumnType[_]] = None
 //    langtag
 //    cell
 //    row
-)
+) {
+
+  def merge(columnType: ColumnType[_]): ComparisonObjects = {
+    new ComparisonObjects(this.tableOpt, Some(columnType))
+  }
+}
