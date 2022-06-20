14cb23b2122a04ca06dff2c56d275d37d2814c8f
Thimo Schneider <schneider.thimo@protonmail.com>
2022-06-20 17:28:35 +0200

    Add new schema for link_table update

--- src/main/resources/schema/schema_v31.sql
+++ src/main/resources/schema/schema_v31.sql
@@ -1,11 +1,11 @@
-do
-$$
-declare
-	row record;
-begin
-	for row in select table_name from information_schema.tables where table_name like 'link_table_%'
-	loop 
-		execute 'ALTER TABLE public.' || quote_ident(row.table_name) || ' ADD COLUMN links_from jsonb DEFAULT NULL;';
-	end loop;
-end;
-$$;	
+CREATE OR REPLACE FUNCTION update_link_tables(linkid BIGINT)
+RETURNS TEXT AS $$
+BEGIN
+
+EXECUTE 'ALTER TABLE public.link_table_' || linkid || ' ADD COLUMN links_from jsonb DEFAULT NULL;';
+RETURN 'link_table_' || linkid :: TEXT;
+END
+$$ LANGUAGE plpgsql;
+
+SELECT update_link_tables(link_id)
+FROM system_link_table;
