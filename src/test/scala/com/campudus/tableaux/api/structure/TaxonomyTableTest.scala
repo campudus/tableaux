5bdf034e002b5ee5be8bef673cb130786029f0b9
Hermann Pauly <hp@campudus.com>
2022-10-11 10:53:37 +0200

    Actually commit linter results

--- src/test/scala/com/campudus/tableaux/api/structure/TaxonomyTableTest.scala
+++ src/test/scala/com/campudus/tableaux/api/structure/TaxonomyTableTest.scala
@@ -3,15 +3,17 @@ package com.campudus.tableaux.api.structure
 import com.campudus.tableaux.database.model.TableauxModel.TableId
 import com.campudus.tableaux.testtools.JsonAssertable
 import com.campudus.tableaux.testtools.TableauxTestBase
+
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.unit.TestContext
 import io.vertx.ext.unit.junit.VertxUnitRunner
-import org.junit.Assert._
-import org.junit.Test
-import org.junit.runner.RunWith
 import org.vertx.scala.core.json.Json
 
 import scala.concurrent.Future
+
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.runner.RunWith
 
 @RunWith(classOf[VertxUnitRunner])
 class TaxonomyTableTest extends TableauxTestBase {
