a26b326222b54ff25430f4d149cdfba2b73666b1
Thimo <schneider.thimo@protonmail.com>
2023-02-24 03:04:48 +0100

    Fix db versions

--- src/main/scala/com/campudus/tableaux/database/model/SystemModel.scala
+++ src/main/scala/com/campudus/tableaux/database/model/SystemModel.scala
@@ -159,7 +159,8 @@ class SystemModel(override protected[this] val connection: DatabaseConnection) e
     setupVersion(readSchemaFile("schema_v30"), 30),
     setupVersion(readSchemaFile("schema_v31"), 31),
     setupVersion(readSchemaFile("schema_v32"), 32),
-    setupVersion(readSchemaFile("schema_v34"), 33)
+    setupVersion(readSchemaFile("schema_v33"), 33),
+    setupVersion(readSchemaFile("schema_v34"), 34)
   )
 
   private def readSchemaFile(name: String): String = {
