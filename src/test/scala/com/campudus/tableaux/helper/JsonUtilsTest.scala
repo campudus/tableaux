4cd73ae34ba6f809dd303c75cfabc558241cf0fc
Manfred Zingl <mz@campudus.com>
2025-05-09 18:24:20 +0200

    again fix more tests

--- src/test/scala/com/campudus/tableaux/helper/JsonUtilsTest.scala
+++ src/test/scala/com/campudus/tableaux/helper/JsonUtilsTest.scala
@@ -10,13 +10,13 @@ import org.junit.{Assert, Test}
 import org.junit.runner.RunWith
 
 @RunWith(classOf[VertxUnitRunner])
-class RejectNonChanges extends TableauxTestBase {
+class OmitNonChanges extends TableauxTestBase {
 
   @Test
-  def rejectEqualValues(): Unit = {
+  def omitEqualValues(): Unit = {
     val oldMap = Map("de" -> Some("HallWrong"), "en" -> Some("Hello"))
     val newMap = Map("de" -> Some("Hallo"), "en" -> Some("Hello"))
-    val result = JsonUtils.rejectNonChanges(newMap, oldMap)
+    val result = JsonUtils.omitNonChanges(newMap, oldMap)
 
     Assert.assertEquals(Map("de" -> Some("Hallo")), result)
   }
@@ -25,27 +25,36 @@ class RejectNonChanges extends TableauxTestBase {
   def ignoreAdditionalValuesInOldMap(): Unit = {
     val oldMap = Map("de" -> Some("HallWrong"), "en" -> Some("Hello"), "fr" -> Some("Bonjour"))
     val newMap = Map("de" -> Some("Hallo"), "en" -> Some("Hello"))
-    val result = JsonUtils.rejectNonChanges(newMap, oldMap)
+    val result = JsonUtils.omitNonChanges(newMap, oldMap)
 
     Assert.assertEquals(Map("de" -> Some("Hallo")), result)
   }
 
   @Test
-  def doNotRejectNullValuesInNewMap(): Unit = {
+  def doNotOmitNullValuesInNewMap(): Unit = {
     val oldMap = Map("de" -> Some("Hallo"), "en" -> Some("Hello"))
     val newMap = Map("de" -> Some("Hallo"), "en" -> None)
-    val result = JsonUtils.rejectNonChanges(newMap, oldMap)
+    val result = JsonUtils.omitNonChanges(newMap, oldMap)
 
     Assert.assertEquals(Map("en" -> None), result)
   }
 
   @Test
-  def returnEmptyMapIfAllKeysAreRejected(): Unit = {
+  def returnEmptyMapIfAllKeysAreOmitted(): Unit = {
     val oldMap = Map("de" -> Some("Hallo"), "en" -> Some("Hello"))
     val newMap = Map("de" -> Some("Hallo"), "en" -> Some("Hello"))
-    val result = JsonUtils.rejectNonChanges(newMap, oldMap)
+    val result = JsonUtils.omitNonChanges(newMap, oldMap)
 
     Assert.assertEquals(Map.empty[String, Option[Any]], result)
+  }
+
+  @Test
+  def doOmitNullValuesIfNotExistsInOldMap(): Unit = {
+    val oldMap = Map("DE" -> Some("200"), "GB" -> Some("300"))
+    val newMap = Map("DE" -> Some("400"), "FR" -> Option(null))
+    val result = JsonUtils.omitNonChanges(newMap, oldMap)
+
+    Assert.assertEquals(Map("DE" -> Some("400")), result)
   }
 }
 
