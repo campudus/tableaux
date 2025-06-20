4fbdd6041c737d5699ee28c6479ae73d637759a5
smnhgn <simon.haugen@campudus.com>
2025-06-20 13:47:34 +0200

    feat: add setting validation schemas

--- src/main/resources/schema/schema_v41.sql
+++ src/main/resources/schema/schema_v41.sql
@@ -42,3 +42,167 @@ CREATE TABLE user_settings_filter (
   PRIMARY KEY (id),
   FOREIGN KEY (key) REFERENCES user_setting_schemas (key) ON DELETE CASCADE
 );
+
+DO $$
+DECLARE
+  boolean_schema jsonb := '{
+    "$schema": "http://json-schema.org/draft-07/schema",
+    "type":"object",
+    "required": ["value"],
+    "additionalProperties": false,
+    "properties": {
+      "value": {
+        "type": "boolean"
+      }
+    }
+  }';
+  string_schema jsonb := '{
+    "$schema": "http://json-schema.org/draft-07/schema",
+    "type":"object",
+    "required": ["value"],
+    "additionalProperties": false,
+    "properties": {
+      "value": {
+        "type": "string"
+      }
+    }
+  }';
+  integer_record_schema jsonb := '{
+    "$schema": "http://json-schema.org/draft-07/schema",
+    "type":"object",
+    "required": ["value"],
+    "additionalProperties": false,
+    "properties": {
+      "value": {
+        "type": "object",
+        "patternProperties": {
+          "^[0-9]+$": {
+            "type": "integer"
+          }
+        }
+      }
+    }
+  }';
+  integer_array_schema jsonb := '{
+    "$schema": "http://json-schema.org/draft-07/schema",
+    "type":"object",
+    "required": ["value"],
+    "additionalProperties": false,
+    "properties": {
+      "value": {
+        "type": "array",
+        "items": {
+          "type": "integer"
+        }
+      }
+    }
+  }';
+  id_index_array_schema jsonb := '{
+    "$schema": "http://json-schema.org/draft-07/schema",
+    "type":"object",
+    "required": ["value"],
+    "additionalProperties": false,
+    "properties": {
+      "value": {
+        "type": "array",
+        "items": {
+          "type": "object",
+          "properties": {
+            "id": { "type": "integer" },
+            "idx": { "type": "integer" }
+          },
+          "required": ["id", "idx"],
+          "additionalProperties": false
+        }
+      }
+    },
+  }';
+  filter_schema jsonb := '{
+    "$schema": "http://json-schema.org/draft-07/schema",
+    "type": "object",
+    "required": [
+      "value"
+    ],
+    "additionalProperties": false,
+    "properties": {
+      "value": {
+        "type": "object",
+        "properties": {
+          "sortDirection": {
+            "type": "string",
+            "enum": [
+              "asc",
+              "desc"
+            ]
+          },
+          "sortColumnName": {
+            "type": "string"
+          },
+          "filters": {
+            "$ref": "#/definitions/filter"
+          }
+        }
+      }
+    },
+    "definitions": {
+      "filter": {
+        "anyOf": [
+          {
+            "type": "array",
+            "minItems": 4,
+            "maxItems": 4,
+            "items": [
+              {
+                "type": "string"
+              },
+              {
+                "type": "string"
+              },
+              {
+                "type": "string"
+              },
+              {
+                "type": "string"
+              }
+            ]
+          },
+          {
+            "type": "array",
+            "minItems": 2,
+            "items": [
+              {
+                "type": "string",
+                "enum": [
+                  "and",
+                  "or"
+                ]
+              }
+            ],
+            "additionalItems": {
+              "$ref": "#/definitions/filter"
+            }
+          }
+        ]
+      }
+    }
+  }';
+
+BEGIN
+  INSERT INTO user_setting_schemas
+    (key, schema)
+  VALUES
+    -- global setting schemas
+    ('filterReset', boolean_schema),
+    ('columnsReset', boolean_schema),
+    ('annotationReset', boolean_schema),
+    ('sortingReset', boolean_schema),
+    ('sortingDesc', boolean_schema),
+    ('markdownEditor', string_schema),
+    -- tableView setting schemas
+    ('columnWidths', integer_record_schema),
+    ('annotationHighlight', string_schema),
+    ('visibleColumns', integer_array_schema),
+    ('columnOrdering', id_index_array_schema),
+    ('rowsFilter', filter_schema)
+  ;
+END $$ LANGUAGE plpgsql;
