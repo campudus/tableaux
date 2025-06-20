2bfa4dcac834e66387a45d780ccc7ffbd0711a3e
smnhgn <simon.haugen@campudus.com>
2025-06-20 11:10:08 +0200

    feat: add user setting update endpoint with validation

--- src/main/resources/schema/schema_v41.sql
+++ src/main/resources/schema/schema_v41.sql
@@ -1,3 +1,12 @@
+CREATE TABLE user_setting_schemas (
+  key           VARCHAR(255) NOT NULL,
+  schema        JSONB,
+  created_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at    TIMESTAMP WITHOUT TIME ZONE,
+
+  PRIMARY KEY (key)
+);
+
 CREATE TABLE user_settings_global (
   key           VARCHAR(255) NOT NULL,
   user_id       VARCHAR(255) NOT NULL,
@@ -5,7 +14,8 @@ CREATE TABLE user_settings_global (
   created_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at    TIMESTAMP WITHOUT TIME ZONE,
 
-  PRIMARY KEY (key, user_id)
+  PRIMARY KEY (key, user_id),
+  FOREIGN KEY (key) REFERENCES user_setting_schemas (key) ON DELETE CASCADE
 );
 
 CREATE TABLE user_settings_table (
@@ -16,16 +26,19 @@ CREATE TABLE user_settings_table (
   created_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at    TIMESTAMP WITHOUT TIME ZONE,
 
-  PRIMARY KEY (key, user_id, table_id)
+  PRIMARY KEY (key, user_id, table_id),
+  FOREIGN KEY (key) REFERENCES user_setting_schemas (key) ON DELETE CASCADE
 );
 
 CREATE TABLE user_settings_filter (
   id            BIGSERIAL NOT NULL,
+  key           VARCHAR(255) NOT NULL,
   user_id       VARCHAR(255) NOT NULL,
   name          VARCHAR(255) NOT NULL,
   value         JSONB,
   created_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at    TIMESTAMP WITHOUT TIME ZONE,
 
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (key) REFERENCES user_setting_schemas (key) ON DELETE CASCADE
 );
