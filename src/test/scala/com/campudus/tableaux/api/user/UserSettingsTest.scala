9760870edc20a78256597d2f3150d7209b28cd0f
smnhgn <simon.haugen@campudus.com>
2025-06-24 11:03:02 +0200

    feat: add delete endpoint with tests

--- src/test/scala/com/campudus/tableaux/api/user/UserSettingsTest.scala
+++ src/test/scala/com/campudus/tableaux/api/user/UserSettingsTest.scala
@@ -174,4 +174,110 @@ class UserSettingsTest extends TableauxTestBase {
       ))
     )
   }
+
+  @Test
+  def testRetrieveSettings(implicit c: TestContext): Unit = okTest {
+    for {
+      // create some settings
+      _ <- sendRequest("PUT", "/user/settings/filterReset", Json.obj("value" -> true))
+      _ <- sendRequest("PUT", "/user/settings/sortingReset", Json.obj("value" -> false))
+      _ <- sendRequest("PUT", "/user/settings/sortingDesc", Json.obj("value" -> false))
+      _ <- sendRequest(
+        "PUT",
+        "/user/settings/visibleColumns?tableId=1",
+        Json.obj("value" -> Json.arr(1, 2, 3))
+      )
+      _ <- sendRequest(
+        "PUT",
+        "/user/settings/rowsFilter?tableId=2",
+        Json.obj("value" -> Json.obj("filters" -> Json.arr("value", "ID", "contains", "Dusk")))
+      )
+      _ <- sendRequest(
+        "PUT",
+        "/user/settings/presetFilter?name=simple",
+        Json.obj("value" -> Json.obj("filters" -> Json.arr("value", "identifier", "starts-with", "black")))
+      )
+      // retrieve settings
+      allSettings <- sendRequest("GET", "/user/settings")
+      globalSettings <- sendRequest("GET", "/user/settings?kind=global")
+      tableSettings <- sendRequest("GET", "/user/settings?kind=table")
+      filterSettings <- sendRequest("GET", "/user/settings?kind=filter")
+    } yield {
+      assertEquals(6, allSettings.getJsonArray("settings").size())
+      assertEquals(3, globalSettings.getJsonArray("settings").size())
+      assertEquals(2, tableSettings.getJsonArray("settings").size())
+      assertEquals(1, filterSettings.getJsonArray("settings").size())
+    }
+  }
+
+  @Test
+  def testDeleteGlobalSetting(implicit c: TestContext): Unit = okTest {
+    for {
+      _ <- sendRequest("PUT", "/user/settings/filterReset", Json.obj("value" -> true))
+      globalSettingsAfterCreate <- sendRequest("GET", "/user/settings?kind=global")
+      _ <- sendRequest("DELETE", "/user/settings/filterReset")
+      globalSettingsAfterDelete <- sendRequest("GET", "/user/settings?kind=global")
+    } yield {
+      assertEquals(1, globalSettingsAfterCreate.getJsonArray("settings").size())
+      assertEquals(0, globalSettingsAfterDelete.getJsonArray("settings").size())
+    }
+  }
+
+  @Test
+  def testDeleteTableSetting(implicit c: TestContext): Unit = okTest {
+    for {
+      _ <- sendRequest(
+        "PUT",
+        "/user/settings/visibleColumns?tableId=1",
+        Json.obj("value" -> Json.arr(1, 2, 3))
+      )
+      tableSettingsAfterCreate <- sendRequest("GET", "/user/settings?kind=table")
+      _ <- sendRequest("DELETE", "/user/settings/visibleColumns?tableId=1")
+      tableSettingsAfterDelete <- sendRequest("GET", "/user/settings?kind=table")
+    } yield {
+      assertEquals(1, tableSettingsAfterCreate.getJsonArray("settings").size())
+      assertEquals(0, tableSettingsAfterDelete.getJsonArray("settings").size())
+    }
+  }
+
+  @Test
+  def testDeleteTableSettingNoTableId(implicit c: TestContext): Unit = exceptionTest("error.arguments") {
+    for {
+      _ <- sendRequest(
+        "PUT",
+        "/user/settings/visibleColumns?tableId=1",
+        Json.obj("value" -> Json.arr(1, 2, 3))
+      )
+      _ <- sendRequest("DELETE", "/user/settings/visibleColumns")
+    } yield ()
+  }
+
+  @Test
+  def testDeleteFilterSetting(implicit c: TestContext): Unit = okTest {
+    for {
+      settingId <- sendRequest(
+        "PUT",
+        "/user/settings/presetFilter?name=simple",
+        Json.obj("value" -> Json.obj("filters" -> Json.arr("value", "identifier", "starts-with", "black")))
+      ).map(_.getLong(("id")))
+      filterSettingsAfterCreate <- sendRequest("GET", "/user/settings?kind=filter")
+      _ <- sendRequest("DELETE", s"/user/settings/presetFilter?id=$settingId")
+      filterSettingsAfterDelete <- sendRequest("GET", "/user/settings?kind=filter")
+    } yield {
+      assertEquals(1, filterSettingsAfterCreate.getJsonArray("settings").size())
+      assertEquals(0, filterSettingsAfterDelete.getJsonArray("settings").size())
+    }
+  }
+
+  @Test
+  def testDeleteFilterSettingNoId(implicit c: TestContext): Unit = exceptionTest("error.arguments") {
+    for {
+      _ <- sendRequest(
+        "PUT",
+        "/user/settings/presetFilter?name=simple",
+        Json.obj("value" -> Json.obj("filters" -> Json.arr("value", "identifier", "starts-with", "black")))
+      )
+      _ <- sendRequest("DELETE", s"/user/settings/presetFilter")
+    } yield ()
+  }
 }
