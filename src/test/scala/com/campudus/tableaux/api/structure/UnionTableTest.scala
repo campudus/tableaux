eaa0e62dab36fbadcfea9769668b9f39bec9c6e4
Manfred Zingl <mz@campudus.com>
2025-10-13 15:49:09 +0200

    add a link column to union table

--- src/test/scala/com/campudus/tableaux/api/structure/UnionTableTest.scala
+++ src/test/scala/com/campudus/tableaux/api/structure/UnionTableTest.scala
@@ -1,9 +1,13 @@
 package com.campudus.tableaux.api.structure
 
+import com.campudus.tableaux.database.domain.Cardinality
+import com.campudus.tableaux.database.domain.Constraint
+import com.campudus.tableaux.database.model.TableauxModel.ColumnId
 import com.campudus.tableaux.database.model.TableauxModel.TableId
-import com.campudus.tableaux.testtools.RequestCreation
+import com.campudus.tableaux.testtools.RequestCreation._
 import com.campudus.tableaux.testtools.TableauxTestBase
 
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.unit.TestContext
 import io.vertx.ext.unit.junit.VertxUnitRunner
 import org.vertx.scala.core.json.Json
@@ -19,13 +23,23 @@ class UnionTableTest extends TableauxTestBase {
 
   def createTableJson(name: String) = Json.obj("name" -> name)
 
-  def createTextColumnJson(name: String) = RequestCreation.Columns().add(RequestCreation.TextCol(name)).getJson
+  def createTextColumnJson(name: String) = Columns().add(Identifier(TextCol(name))).getJson
 
-  def createMultilangTextColumnJson(name: String) =
-    RequestCreation.Columns().add(RequestCreation.Multilanguage(RequestCreation.TextCol(name))).getJson
+  def createMultilangTextColumnJson(name: String) = Columns().add(Multilanguage(TextCol(name))).getJson
 
   def createNumberColumnJson(name: String, decimalDigits: Option[Int] = None) =
-    RequestCreation.Columns().add(RequestCreation.NumericCol(name, decimalDigits)).getJson
+    Columns().add(NumericCol(name, decimalDigits)).getJson
+
+  def createCardinalityLinkColumn(
+      toTableId: TableId,
+      name: String,
+      from: Int,
+      to: Int
+  ) = Columns(LinkBiDirectionalCol(
+    name,
+    toTableId,
+    Constraint(Cardinality(from, to), deleteCascade = false)
+  )).getJson
 
   def fetchTable(tableId: TableId) = sendRequest("GET", s"/completetable/$tableId")
 
@@ -36,47 +50,55 @@ class UnionTableTest extends TableauxTestBase {
    * 
    * - column "name" is of type text, single language
    * - column "color" is of type text, multi language
-   * - column "glossLevel" is of type number, single language
+   * - column "prio" is of type number, single language
+   * - column "glossLevel" is of type link to same table
    *
-   * table1 ┌──┬────┬─────┬──────────┐
-   *        │id│name│color│glossLevel│
-   *        ├──┼────┼─────┼──────────┤
-   *        │0 │1   │2    │3         │
-   *        └──┴────┴─────┴──────────┘
-   * table2 ┌──┬────┬─────┬──────────┐
-   *        │id│name│color│glossLevel│
-   *        ├──┼────┼─────┼──────────┤
-   *        │0 │1   │3    │2         │
-   *        └──┴────┴─────┴──────────┘
-   * table3 ┌──┬────┬─────┬──────────┐
-   *        │id│name│color│glossLevel│
-   *        ├──┼────┼─────┼──────────┤
-   *        │0 │3   │2    │1         │
-   *        └──┴────┴─────┴──────────┘
+   * table1 ┌───┬─────┬──────┬─────┬───────────┐
+   *        │id │name │color │prio │glossLevel │
+   *        ├───┼─────┼──────┼─────┼───────────┤
+   *        │0  │1    │2     │3    │4          │
+   *        └───┴─────┴──────┴─────┴───────────┘
+   * table2 ┌───┬─────┬──────┬─────┬───────────┐
+   *        │id │name │color │prio │glossLevel │
+   *        ├───┼─────┼──────┼─────┼───────────┤
+   *        │0  │1    │3     │4    │2          │
+   *        └───┴─────┴──────┴─────┴───────────┘
+   * table3 ┌───┬─────┬──────┬─────┬───────────┐
+   *        │id │name │color │prio │glossLevel │
+   *        ├───┼─────┼──────┼─────┼───────────┤
+   *        │0  │4    │2     │3    │1          │
+   *        └───┴─────┴──────┴─────┴───────────┘
    */
   // format: on
   private def createUnionTable(): Future[TableId] = {
+
     val createColumnName = createTextColumnJson("name")
     val createColumnColor = createMultilangTextColumnJson("color")
-    val createColumnGloss = createNumberColumnJson("glossLevel")
+    val createColumnPrio = createNumberColumnJson("prio")
 
     for {
+      glossLinkTableId <- createDefaultTable(name = "glossLink")
+      createColumnGloss = createCardinalityLinkColumn(glossLinkTableId, "glossLevel", 1, 0)
+
       tableId1 <- sendRequest("POST", "/tables", createTableJson("table1")).map(_.getLong("id"))
       tableId2 <- sendRequest("POST", "/tables", createTableJson("table2")).map(_.getLong("id"))
       tableId3 <- sendRequest("POST", "/tables", createTableJson("table3")).map(_.getLong("id"))
 
       // create columns in tables in different order
-      _ <- sendRequest("POST", s"/tables/1/columns", createColumnName)
-      _ <- sendRequest("POST", s"/tables/1/columns", createColumnColor)
-      _ <- sendRequest("POST", s"/tables/1/columns", createColumnGloss)
+      _ <- sendRequest("POST", s"/tables/$tableId1/columns", createColumnName)
+      _ <- sendRequest("POST", s"/tables/$tableId1/columns", createColumnColor)
+      _ <- sendRequest("POST", s"/tables/$tableId1/columns", createColumnPrio)
+      _ <- sendRequest("POST", s"/tables/$tableId1/columns", createColumnGloss)
 
-      _ <- sendRequest("POST", s"/tables/2/columns", createColumnName)
-      _ <- sendRequest("POST", s"/tables/2/columns", createColumnGloss)
-      _ <- sendRequest("POST", s"/tables/2/columns", createColumnColor)
+      _ <- sendRequest("POST", s"/tables/$tableId2/columns", createColumnName)
+      _ <- sendRequest("POST", s"/tables/$tableId2/columns", createColumnGloss)
+      _ <- sendRequest("POST", s"/tables/$tableId2/columns", createColumnColor)
+      _ <- sendRequest("POST", s"/tables/$tableId2/columns", createColumnPrio)
 
-      _ <- sendRequest("POST", s"/tables/3/columns", createColumnGloss)
-      _ <- sendRequest("POST", s"/tables/3/columns", createColumnColor)
-      _ <- sendRequest("POST", s"/tables/3/columns", createColumnName)
+      _ <- sendRequest("POST", s"/tables/$tableId3/columns", createColumnGloss)
+      _ <- sendRequest("POST", s"/tables/$tableId3/columns", createColumnColor)
+      _ <- sendRequest("POST", s"/tables/$tableId3/columns", createColumnPrio)
+      _ <- sendRequest("POST", s"/tables/$tableId3/columns", createColumnName)
 
       payload = Json.obj(
         "name" -> "union",
@@ -149,40 +171,66 @@ class UnionTableTest extends TableauxTestBase {
 
   @Test
   def addSimpleColumnsToUnionTable(implicit c: TestContext): Unit = okTest {
+    import scala.collection.JavaConverters._
+
     val unionTableCol1 = Json.obj(
+      "name" -> "originTable",
+      "kind" -> "text",
+      "ordering" -> 1,
+      "displayName" -> Json.obj("de" -> "Ursprungstabelle", "en" -> "Origin Table"),
+      "description" -> Json.obj(
+        "de" -> "Der Tabellenname, aus der die Daten stammen",
+        "en" -> "The name of the table from which the data is taken"
+      )
+    )
+
+    val unionTableCol2 = Json.obj(
       "name" -> "name",
       "kind" -> "text",
       "ordering" -> 1,
       "displayName" -> Json.obj("de" -> "Marketing Name", "en" -> "Marketing Name"),
       "description" -> Json.obj("de" -> "Marketingname der Farbe", "en" -> "marketing name of the color"),
       "originColumns" -> Json.obj(
-        "1" -> Json.obj("id" -> 1),
         "2" -> Json.obj("id" -> 1),
-        "3" -> Json.obj("id" -> 3)
+        "3" -> Json.obj("id" -> 1),
+        "4" -> Json.obj("id" -> 4)
       )
     )
-    val unionTableCol2 = Json.obj(
+    val unionTableCol3 = Json.obj(
       "name" -> "color",
       "kind" -> "text",
       "ordering" -> 2,
       "displayName" -> Json.obj("de" -> "Name der Farbe", "en" -> "Color Name"),
       "description" -> Json.obj("de" -> "Name der Farbe", "en" -> "Name of the color"),
       "originColumns" -> Json.obj(
-        "1" -> Json.obj("id" -> 2),
-        "2" -> Json.obj("id" -> 3),
-        "3" -> Json.obj("id" -> 2)
+        "2" -> Json.obj("id" -> 2),
+        "3" -> Json.obj("id" -> 3),
+        "4" -> Json.obj("id" -> 2)
       )
     )
-    val unionTableCol3 = Json.obj(
+    val unionTableCol4 = Json.obj(
+      "name" -> "prio",
+      "kind" -> "numeric",
+      "ordering" -> 3,
+      "displayName" -> Json.obj("de" -> "Prio", "en" -> "Priority"),
+      "description" -> Json.obj("de" -> "Die Priorität der Farbe", "en" -> "The priority of the color"),
+      "originColumns" -> Json.obj(
+        "2" -> Json.obj("id" -> 3),
+        "3" -> Json.obj("id" -> 4),
+        "4" -> Json.obj("id" -> 3)
+      )
+    )
+    val unionTableCol5 = Json.obj(
       "name" -> "glossLevel",
       "kind" -> "numeric",
+      // "kind" -> "numeric",
       "ordering" -> 3,
       "displayName" -> Json.obj("de" -> "Glanzgrad", "en" -> "Gloss Level"),
       "description" -> Json.obj("de" -> "Der Glanzgrad der Farbe", "en" -> "The gloss level of the color"),
       "originColumns" -> Json.obj(
-        "1" -> Json.obj("id" -> 3),
-        "2" -> Json.obj("id" -> 2),
-        "3" -> Json.obj("id" -> 1)
+        "2" -> Json.obj("id" -> 4),
+        "3" -> Json.obj("id" -> 2),
+        "4" -> Json.obj("id" -> 1)
       )
     )
 
@@ -192,24 +240,31 @@ class UnionTableTest extends TableauxTestBase {
       columns <- sendRequest(
         "POST",
         s"/tables/$tableId/columns",
-        Json.obj("columns" -> Json.arr(unionTableCol1, unionTableCol2, unionTableCol3))
+        Json.obj("columns" -> Json.arr(unionTableCol2, unionTableCol3, unionTableCol4, unionTableCol5))
       )
 
-      // unionTable <- fetchTable(tableId)
-      unionColumns <- fetchColumns(tableId)
+      unionTable <- fetchTable(tableId)
+      unionColumns <- fetchColumns(tableId).map(_.getJsonArray("columns")).map(_.asScala.collect({
+        case obj: JsonObject => obj
+      }).toSeq)
     } yield {
-      // val expectedColumn = Json.obj(
-      //   "name" -> "test",
-      //   "kind" -> "unionsimple",
-      //   "displayName" -> Json.obj("de" -> "Test"),
-      //   "description" -> Json.obj("de" -> "")
-      // )
+      val expectedTable = Json.obj(
+        "id" -> 5,
+        "name" -> "union",
+        "hidden" -> false,
+        "displayName" -> Json.obj("de" -> "Union Table"),
+        "langtags" -> Json.arr("de-DE", "en-GB"),
+        "type" -> "union",
+        "originTables" -> Json.arr(2, 4, 3)
+      )
+      assertJSONEquals(expectedTable, unionTable)
 
-      // println(s"unionTable: $unionTable")
-      println(s"column: $columns")
-      println(s"unionColumns: $unionColumns")
-
-      // assertEquals(expectedColumn, column.getJsonArray("columns").getJsonObject(0))
+      assertEquals(5, unionColumns.size)
+      assertJSONEquals(unionTableCol1, unionColumns(0))
+      assertJSONEquals(unionTableCol2, unionColumns(1))
+      assertJSONEquals(unionTableCol3, unionColumns(2))
+      assertJSONEquals(unionTableCol4, unionColumns(3))
+      assertJSONEquals(unionTableCol5, unionColumns(4))
     }
   }
 }
