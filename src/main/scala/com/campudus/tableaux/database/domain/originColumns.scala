038fe700d252127232df5cd90544ca4bd9d13bc0
Manfred Zingl <mz@campudus.com>
2025-10-30 19:11:54 +0100

    not good but working somehow

--- src/main/scala/com/campudus/tableaux/database/domain/originColumns.scala
+++ src/main/scala/com/campudus/tableaux/database/domain/originColumns.scala
@@ -1,6 +1,5 @@
 package com.campudus.tableaux.database.domain
 
-import com.campudus.tableaux.database.domain.DisplayInfos._
 import com.campudus.tableaux.database.model.TableauxModel._
 
 import org.vertx.scala.core.json._
@@ -43,5 +42,44 @@ object OriginColumns {
     }
 
     OriginColumns(tableToColumn)
+  }
+}
+
+case class CreateOriginColumns(tableToColumn: Map[TableId, ColumnId]) {
+
+  def getJson: JsonObject = {
+    val arr = Json.arr(tableToColumn.map { case (tableId, columnId) =>
+      Json.obj("tableId" -> tableId, "columnId" -> columnId)
+    }.toSeq: _*)
+    Json.obj(OriginColumns.fieldName -> arr)
+  }
+}
+
+object CreateOriginColumns {
+  val fieldName = "originColumns"
+
+  def getFieldNames(json: JsonObject): Seq[String] = {
+    import scala.collection.JavaConverters._
+
+    if (json.fieldNames().size() > 0) {
+      json.fieldNames().asScala.toSeq
+    } else {
+      Seq.empty
+    }
+  }
+
+  def fromJson(json: JsonArray = Json.emptyArr()): CreateOriginColumns = {
+    val tableToColumn: Map[TableId, ColumnId] = (json.asScala).map(json => {
+      val singleMapping = json.asInstanceOf[JsonObject]
+      val tableId: TableId = singleMapping.getLong("tableId")
+      val columnId: ColumnId = singleMapping.getLong("columnId")
+      (tableId, columnId)
+    }).toMap
+
+    if (tableToColumn.isEmpty) {
+      throw new IllegalArgumentException(s"Origin columns must not be empty in $json")
+    }
+
+    CreateOriginColumns(tableToColumn)
   }
 }
